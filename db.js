2986344f183947ae7626f0c22b40515f330c05e2
gk <198441850+02preet@users.noreply.github.com>
2025-05-19 14:46:16 +1000

    Fixed errors

--- db.js
+++ db.js
@@ -8,7 +8,7 @@ const connection = mysql.createConnection({
   host: 'localhost',   // Your MySQL host (use '127.0.0.1' or 'localhost')
   user: 'root',        // Your MySQL username
   password: '',  // Your MySQL password
-  database: 'iot'      // The database you want to connect to
+  //database: 'iot'      // The database you want to connect to
 });
 
 // Connect to the database
