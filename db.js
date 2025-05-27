5b6b4edf22c9e6f0ef414eb63d52c102cb4bfdf7
Camby10101 <129371743+Camby10101@users.noreply.github.com>
2025-05-27 11:50:43 +1000

    Update db.js

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Balllife45',
+  password: '',
   database: 'iot'
 });
 
